82823f386526583d7cee6578bd9d9fc25e8c1f2d
ra-marukun <relu110relu@gmail.com>
2023-06-17 08:28:22 +0900

    chapter13:  add ダイクストラの最短距離更新アルゴリズム  List.mapでfuncを再帰させる時、引数を固定させることで複数引数のfuncを回す

--- metro.ml
+++ metro.ml
@@ -849,3 +849,51 @@ let test4 =
       };
       { kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸ノ内線" };
     ]
+
+(* 直前に確定した駅 p: ekiType と未確定の駅 q: ekiTypeを受け取り、pとqが直接つながっているかを調べ、つながっていた場合qの最短距離と手前リストを更新。つながっていない場合はqをそのまま返す *)
+let koushin1 p q =
+  match p with
+  | { namae = namae_p; saitan_kyori = kyori_p; temae_list = temae_p } -> (
+      match q with
+      | { namae = namae_q; saitan_kyori = kyori_q; temae_list = temae_q } ->
+          let kyori_p_q = get_ekikan_kyori namae_p namae_q global_ekikan_list in
+          if kyori_p_q = infinity then q
+          else
+            let new_kyori_q = kyori_p +. kyori_p_q in
+            if new_kyori_q < kyori_q then
+              {
+                namae = namae_q;
+                saitan_kyori = new_kyori_q;
+                temae_list = namae_q :: temae_p;
+              }
+            else q)
+
+(* 直前に確定した駅pと未確定の駅のリストvを受け取り、更新を行った後の未確定の駅リストを返す *)
+(* List.mapを複数引数の関数に適用したい場合は func p q をlet f q = func p qとしてfuncの引数を確定さた関数fを定義したのち、List.map f lstする（lstはqのリスト） *)
+let koushin p v =
+  let f q = koushin1 p q in
+  List.map f v
+
+(* 駅の例 *)
+let eki1 = { namae = "池袋"; saitan_kyori = infinity; temae_list = [] }
+
+let eki2 = { namae = "新大塚"; saitan_kyori = 1.2; temae_list = [ "新大塚"; "茗荷谷" ] }
+
+let eki3 = { namae = "茗荷谷"; saitan_kyori = 0.; temae_list = [ "茗荷谷" ] }
+
+let eki4 = { namae = "後楽園"; saitan_kyori = infinity; temae_list = [] }
+
+(* 駅リストの例 *)
+let lst = [ eki1; eki2; eki3; eki4 ]
+
+(* テスト *)
+let test1 = koushin eki2 [] = []
+
+let test2 =
+  koushin eki2 lst
+  = [
+      { namae = "池袋"; saitan_kyori = 3.0; temae_list = [ "池袋"; "新大塚"; "茗荷谷" ] };
+      eki2;
+      eki3;
+      eki4;
+    ]
